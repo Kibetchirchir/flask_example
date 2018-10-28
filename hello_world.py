34999a94ff242cb8a897e9b0fcb39f2e7f66091a
Kibet Chirchir <langatchirchir@gmail.com>
2018-10-28 12:13:25 +0300

    added render template

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
+
+post=[
+    {
+        'Author': 'Chirchir',
+        'Title': 'blog post 1',
+        'contents': 'first blog post 1',
+        'date': 'first April 2017'
+    },
+    {
+        'Author': 'Chelangat',
+        'Title': 'blog post 2',
+        'contents': 'first blog post 2',
+        'date': 'first April 2017'
+    }
+
+]
+
 
 @app.route("/")
 def hello():
-    return jsonify("Hello World!")
+    return render_template('home.html', posts=post)
+
+
+@app.route('/home')
+def home():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
